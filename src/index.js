6732642be74aa1f574c898beda42c66f8768e77e
sr-lenin <lenin>
2022-11-22 08:15:40 -0600

    dom methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("funciona");
+const taskForm = document.querySelector('#task-form')
+const taskInput = document.querySelector('#task-form input[name="task"]')
+const todos = document.querySelector('#todo-list')
+
+// crea un elemeneto con el nombre de la etiqueta 
+// const h1 = document.createElement("h1")
+// h1.innerText = "<span>kuk</span>"
+// h1.style.color = "blue"
+
+// agrega un elemento dentro del formulario como su ultimo hijo
+// taskForm.appendChild(h1)
+
+// agraga un elemento dentro del formulario como primer hijo
+// taskForm.prepend(h1)
+
+// agrega un elemento antes del formulario como hermano 
+// taskForm.before(h1)
+
+// agrega un elementpo despues del formulario como hermano
+// taskForm.after(h1)
+
+// elimina un elemento
+// taskForm.remove()
+
+// elimina un elemento de los hijos del formulario
+// taskForm.removeChild(h1)
+
+// agrega una o varias clases al elemento
+// taskForm.classList.add("amigo", 'primo')
+
+// borra una clase ya existente dek elemento
+// taskForm.classList.remove('primo')
+
+// agrega una clase innesistente y/o borra si ya hay una
+// taskForm.classList.toggle('hermano')
+
+// remplaza una clase por otra
+// taskForm.classList.replace('amigo', 'hermana')
+
+// ver propiendades de los ementos
+// console.log(taskForm);
+
+// ver el valor de un input
+// console.log(taskInput.value);
+
+// modificar el valor del input
+// taskInput.value = "fecha"

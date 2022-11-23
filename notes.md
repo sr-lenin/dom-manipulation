edb181ec5f1f4457ed108f8c22ab206cdf6b9ab1
sr-lenin <lenin>
2022-11-23 07:19:49 -0600

    practice dom-manipulation

---
+++ notes.md
@@ -0,0 +1,83 @@
+# Notas
+
+## Creacion de elementos
+crea un elemeneto con el nombre de la etiqueta como se escribe en html
+```javascript
+const boton = document.createElement("button")
+boton.style.color = "blue"
+```
+
+## Agregar un elemento en la pagina
+agrega un elemento dentro del formulario como su ultimo hijo
+```js
+taskForm.appendChild(boton)
+```
+
+agraga un elemento dentro del formulario como primer hijo
+```js
+taskForm.prepend(h1)
+```
+
+agrega un elemento antes del formulario como hermano 
+```js
+taskForm.before(h1)
+```
+
+agrega un elementpo despues del formulario como hermano
+```js
+taskForm.after(h1)
+```
+
+## Quitar un elemento de la pagina
+elimina un elemento
+```js
+taskForm.remove()
+```
+
+elimina un elemento de los hijos del formulario
+```js
+taskForm.removeChild(h1)
+```
+
+## Cambiar las clases CSS de un elemento
+
+agrega una o varias clases al elemento
+```js
+taskForm.classList.add("amigo", 'primo')
+```
+borra una clase ya existente dek elemento
+```js
+taskForm.classList.remove('primo')
+```
+
+agrega una clase innesistente y/o borra si ya hay una
+```js
+taskForm.classList.toggle('hermano')
+```
+
+remplaza una clase por otra
+```js
+taskForm.classList.replace('amigo', 'hermana')
+```
+
+## Propiedades de un elemento
+ver propiendades de los ementos
+```js
+console.log(taskForm);
+```
+
+ver el valor de un input
+```js
+console.log(taskInput.value);
+```
+
+modificar el valor del input
+```js
+taskInput.value = "fecha"
+```
+
+# Agregar Texto
+```js
+const h2Element = document.createElement('h2')
+h2Element.textContent = 'Task Form'
+```
--- src/index.js
+++ src/index.js
@@ -2,46 +2,23 @@ const taskForm = document.querySelector('#task-form')
 const taskInput = document.querySelector('#task-form input[name="task"]')
 const todos = document.querySelector('#todo-list')
 
-// crea un elemeneto con el nombre de la etiqueta 
-// const h1 = document.createElement("h1")
-// h1.innerText = "<span>kuk</span>"
-// h1.style.color = "blue"
+// tarea 1
+const buttonElement = document.createElement("button")
+taskForm.appendChild(buttonElement)
 
-// agrega un elemento dentro del formulario como su ultimo hijo
-// taskForm.appendChild(h1)
+// tarea 2
+const formTittle = document.createElement('h2')
+formTittle.textContent = 'Task Form'
+taskForm.before(formTittle)
 
-// agraga un elemento dentro del formulario como primer hijo
-// taskForm.prepend(h1)
+// tarea 3
+const listTittle = document.createElement('h2')
+listTittle.textContent = 'Todo List'
+listTittle.style.color =  'blue' 
+todos.before(listTittle)
 
-// agrega un elemento antes del formulario como hermano 
-// taskForm.before(h1)
-
-// agrega un elementpo despues del formulario como hermano
-// taskForm.after(h1)
-
-// elimina un elemento
-// taskForm.remove()
-
-// elimina un elemento de los hijos del formulario
-// taskForm.removeChild(h1)
-
-// agrega una o varias clases al elemento
-// taskForm.classList.add("amigo", 'primo')
-
-// borra una clase ya existente dek elemento
-// taskForm.classList.remove('primo')
-
-// agrega una clase innesistente y/o borra si ya hay una
-// taskForm.classList.toggle('hermano')
-
-// remplaza una clase por otra
-// taskForm.classList.replace('amigo', 'hermana')
-
-// ver propiendades de los ementos
-// console.log(taskForm);
-
-// ver el valor de un input
-// console.log(taskInput.value);
-
-// modificar el valor del input
-// taskInput.value = "fecha"
+const inputElement = document.createElement('input')
+inputElement.type = 'date'
+inputElement.value = '2022-11-23'
+// inputElement.value = new Date().toISOString().split('T')[0]
+taskInput.after(inputElement)
